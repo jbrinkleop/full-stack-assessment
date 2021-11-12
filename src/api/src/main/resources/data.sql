f1029730c957c3193cf3e67c5b7bdbdf71197ac4
shivani_mangal <shivanimangaleswaran@gmail.com>
2021-11-13 00:17:08 +0530

    Added db schema and data

--- src/api/src/main/resources/data.sql
+++ src/api/src/main/resources/data.sql
@@ -1,1 +1,6 @@
-INSERT INTO departments(name) values ('engineering');
+INSERT INTO departments(name) values ('Engineering');
+INSERT INTO departments(name) values ('Sales');
+INSERT INTO employees(first_name, last_name, dept_id, email_id, job_title, service_date, end_date) values ('Tom', 'Hardy', 1, 'tomhardy@observepoint.com', 'Director', '2015-04-01', null );
+INSERT INTO employees(first_name, last_name, dept_id, email_id, job_title, service_date, end_date) values ('Jane', 'Doe', 2, 'janedoe@observepoint.com', 'Software Engineer', '2020-04-01', null );
+INSERT INTO contact_details(emp_id, address, contact_no, emergency_contact_name, emergency_contact_no) values ('1', 'Salt Lake City, UT', '123-456-789', 'Jill Hardy', '987-654-321');
+INSERT INTO contact_details(emp_id, address, contact_no, emergency_contact_name, emergency_contact_no) values ('2', 'Pleasant Grove, UT', '103-959-889', 'John Doe', '107-694-231');
--- src/api/src/main/resources/schema.sql
+++ src/api/src/main/resources/schema.sql
@@ -3,3 +3,38 @@ CREATE TABLE departments(
     id int AUTO_INCREMENT  PRIMARY KEY,
     name varchar(1000) NOT NULL
 );
+
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees(
+    id int AUTO_INCREMENT  PRIMARY KEY,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    dept_id int,
+    FOREIGN KEY (dept_id) REFERENCES departments(id),
+    email_id varchar(50) NOT NULL,
+    job_title varchar (100) NOT NULL,
+    emp_status varchar (30) DEFAULT 'Active',
+    service_date date NOT NULL,
+    end_date date
+);
+
+DROP TABLE IF EXISTS contact_details;
+CREATE TABLE contact_details(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    emp_id int,
+    FOREIGN KEY (emp_id) REFERENCES employees(id),
+    address varchar(1000) NOT NULL,
+    contact_no varchar(15) NOT NULL,
+    emergency_contact_name varchar(50) NOT NULL,
+    emergency_contact_no varchar(15) NOT NULL
+);
+
+DROP TABLE IF EXISTS revenues;
+CREATE TABLE revenues(
+    id int AUTO_INCREMENT  PRIMARY KEY,
+    dept_id int,
+    FOREIGN KEY (dept_id) REFERENCES departments(id),
+    quarter int NOT NULL,
+    assessment_date date NOT NULL,
+    revenue double NOT NULL
+);
