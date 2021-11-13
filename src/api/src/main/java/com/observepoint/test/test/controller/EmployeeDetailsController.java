d534346f4e5c3664c917dc50de04a85caf97a84c
shivani_mangal <shivanimangaleswaran@gmail.com>
2021-11-13 17:05:34 +0530

    Added cross origin annotation

--- src/api/src/main/java/com/observepoint/test/test/controller/EmployeeDetailsController.java
+++ src/api/src/main/java/com/observepoint/test/test/controller/EmployeeDetailsController.java
@@ -3,6 +3,7 @@ package com.observepoint.test.test.controller;
 import com.observepoint.test.test.model.Employees;
 import com.observepoint.test.test.service.EmployeeDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/employee")
 public class EmployeeDetailsController {
 
